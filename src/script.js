53c7eae3ec0bde90d3828d1e065287afa547029c
Chris Frazier <chrisfrazier0@users.noreply.github.com>
2022-06-17 02:50:37 -0400

    feat: added script file to page headers

---
+++ src/script.js
@@ -0,0 +1,9 @@
+;(function() {
+    let msg = "%c Howdy 👋"
+    let styles = `
+            display: inline-block;
+            padding: 12px 0;
+            font-size: 14px;
+            font-family: monospace;`
+    console.log(msg, styles)
+})()
