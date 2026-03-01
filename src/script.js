07a3a9ceb6fca8c994614e2525c7268960655c93
Chris Frazier <chrisfrazier0@users.noreply.github.com>
2026-03-01 02:08:09 -0500

    redirect to homepage

--- src/script.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  let msg = "%c Howdy 👋";
-  let styles = `
-            display: inline-block;
-            padding: 12px 0;
-            font-size: 14px;
-            font-family: monospace;`;
-  console.log(msg, styles);
-})();
